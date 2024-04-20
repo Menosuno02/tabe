8445afdcca6e7d444a9251a03936888952144490
Menosuno02 <83469483+Menosuno02@users.noreply.github.com>
2024-04-20 21:44:00 +0200

    Commit

---
+++ ProyectoASPNET/Helpers/HelperCacheMultiplexer.cs
@@ -0,0 +1,22 @@
+﻿using StackExchange.Redis;
+
+namespace ProyectoASPNET.Helpers
+{
+    public class HelperCacheMultiplexer
+    {
+        private static Lazy<ConnectionMultiplexer>
+            CreateConnection = new Lazy<ConnectionMultiplexer>(() =>
+            {
+                string cacheRedisKeys = "cacheredistabe.redis.cache.windows.net:6380,password=4AuLp7ljPXAHbv9awttTsAzuSwDohjhIvAzCaAxB1kI=,ssl=True,abortConnect=False";
+                return ConnectionMultiplexer.Connect(cacheRedisKeys);
+            });
+
+        public static ConnectionMultiplexer Connection
+        {
+            get
+            {
+                return CreateConnection.Value;
+            }
+        }
+    }
+}
