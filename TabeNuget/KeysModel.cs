9cb5db506bbebe497b73f26e547edd38f50ad7b5
Menosuno02 <83469483+Menosuno02@users.noreply.github.com>
2024-05-27 21:39:37 +0200

    Commit

---
+++ TabeNuget/KeysModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TabeNuget
+{
+    public class KeysModel
+    {
+        public string Audience { get; set; }
+        public string CacheRedisKey { get; set; }
+        public string EncryptKey { get; set; }
+        public string GoogleApiKey { get; set; }
+        public string Issuer { get; set; }
+        public string MySql { get; set; }
+        public string SecretKey { get; set; }
+        public string SignalRKey { get; set; }
+        public string StorageAccountKey { get; set; }
+        public string TabeAPI { get; set; }
+        public string TabeMailPass { get; set; }
+    }
+}
